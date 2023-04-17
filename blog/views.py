471242f840a49b13d21b3f71ed4c88562037b6ef
Rudransh Verma <rudranshverma@Rudranshs-Mac-mini.local>
2023-04-17 21:32:05 +0530

    apps url and index page set

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("TheAwesomeStore Blog")
+    return render(request, "blog/index.html")

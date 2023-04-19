1ef834dcb49dc0bd3e3fa3e7bdce13ad315bc44a
Rudransh Verma <rudranshverma@Rudranshs-Mac-mini.local>
2023-04-19 16:08:06 +0530

    product model applied and created superuser

--- store/views.py
+++ store/views.py
@@ -1,5 +1,49 @@
 from django.shortcuts import render
+from django.conf.urls.static import static
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "store/index.html")
+
+
+
+
+
+def about(request):
+    return HttpResponse("You are at about page")
+
+
+
+
+
+def contact(request):
+    return HttpResponse("You are at contact page")
+
+
+
+
+
+def tracker(request):
+    return HttpResponse("You are at tracker page")
+
+
+
+
+
+def search(request):
+    return HttpResponse("You are at search page")
+
+
+
+
+
+def productView(request):
+    return HttpResponse("You are at product view page")
+
+
+
+
+
+def checkout(request):
+    return render(request, "store/index.html")
+

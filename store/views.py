d3669d35db6aca7243b0b943beb10850fb344b25
Rudransh Verma <rudranshverma1984@gmail.com>
2023-04-24 21:58:48 +0530

    template inheritance 2

--- store/views.py
+++ store/views.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 from django.shortcuts import render
 from django.conf.urls.static import static
 from django.http import HttpResponse
@@ -6,10 +8,14 @@ from store.models import Product
 # Create your views here.
 def index(request):
     var = Product.objects.all()
+    n = len(var)
+    nSlides = n//4 + ceil((n/4)-(n//4))
     dict = {
-        'prodname': var,
+        'prodslides': nSlides,
+        'prod': var,
     }
     return render(request, "store/index.html", dict)
+
 
 def about(request):
     return render(request, "store/about.html")

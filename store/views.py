8879d9572f9375fa923d4b43493ac1d3b3ec7023
Rudransh Verma <rudranshverma1984@gmail.com>
2023-05-13 16:53:05 +0530

    cwh-homepagelist

--- store/views.py
+++ store/views.py
@@ -3,52 +3,21 @@ from math import ceil
 from django.shortcuts import render
 from django.conf.urls.static import static
 from django.http import HttpResponse
+from math import ceil
 
 from store.models import Product
 # Create your views here.
 def index(request):
-    var = Product.objects.all()
-    n = len(var)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    dict = {
-        'prodslides': nSlides,
-        'prod': var,
+    products = Product.objects.all()
+    n = len(products)
+    nSlides = n//4 + ceil((n-4)/(n//4))
+    params = {
+        'product': products,
+        'no_of_slides': nSlides,
+        'range': range(1, nSlides)
     }
-    return render(request, "store/index.html", dict)
+    return render(request, "store/index.html", params)
 
 
 def about(request):
     return render(request, "store/about.html")
-
-#
-# def contact(request):
-#     return HttpResponse("You are at contact page")
-#
-#
-#
-#
-#
-# def tracker(request):
-#     return HttpResponse("You are at tracker page")
-#
-#
-#
-#
-#
-# def search(request):
-#     return HttpResponse("You are at search page")
-#
-#
-#
-#
-#
-# def productView(request):
-#     return HttpResponse("You are at product view page")
-#
-#
-#
-#
-#
-# def checkout(request):
-#     return render(request, "store/index.html")
-#

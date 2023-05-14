8bc21821c47e19c04793c7d76b4d9914af905d83
Rudransh Verma <rudranshverma1984@gmail.com>
2023-05-14 21:39:49 +0530

    text

---
+++ store/views.py
@@ -0,0 +1,54 @@
+from math import ceil
+
+from django.shortcuts import render
+from django.conf.urls.static import static
+from django.http import HttpResponse
+
+from store.models import Product
+# Create your views here.
+def index(request):
+    var = Product.objects.all()
+    n = len(var)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    dict = {
+        'prodslides': nSlides,
+        'prod': var,
+    }
+    return render(request, "store/index.html", dict)
+
+
+def about(request):
+    return render(request, "store/about.html")
+
+#
+# def contact(request):
+#     return HttpResponse("You are at contact page")
+#
+#
+#
+#
+#
+# def tracker(request):
+#     return HttpResponse("You are at tracker page")
+#
+#
+#
+#
+#
+# def search(request):
+#     return HttpResponse("You are at search page")
+#
+#
+#
+#
+#
+# def productView(request):
+#     return HttpResponse("You are at product view page")
+#
+#
+#
+#
+#
+# def checkout(request):
+#     return render(request, "store/index.html")
+#

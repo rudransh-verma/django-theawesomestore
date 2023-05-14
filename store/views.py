050df200859ff2ab3eed5d3b291afdc03e9f4c06
Rudransh Verma <rudranshverma1984@gmail.com>
2023-05-14 21:42:49 +0530

    final_push_correction

--- store/views.py
+++ store/views.py
@@ -1,5 +1,3 @@
-from math import ceil
-
 from django.shortcuts import render
 from django.conf.urls.static import static
 from django.http import HttpResponse
@@ -8,14 +6,10 @@ from store.models import Product
 # Create your views here.
 def index(request):
     var = Product.objects.all()
-    n = len(var)
-    nSlides = n//4 + ceil((n/4)-(n//4))
     dict = {
-        'prodslides': nSlides,
-        'prod': var,
+        'prodname': var,
     }
     return render(request, "store/index.html", dict)
-
 
 def about(request):
     return render(request, "store/about.html")

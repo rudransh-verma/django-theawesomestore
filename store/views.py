527b9241002497f71719c7dcd3e48d58acc30d85
Rudransh Verma <rudranshverma1984@gmail.com>
2023-05-14 18:43:54 +0530

    prod

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf.urls.static import static
 from django.http import HttpResponse
+from math import ceil
 
 from store.models import Product
 # Create your views here.

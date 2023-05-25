6721ab5d23d304fa5110b164e4afeb0bd8b10d3d
Rudransh Verma <rudranshverma1984@gmail.com>
2023-05-25 20:24:16 +0530

    home page db fetch carousel complete

--- store/views.py
+++ store/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
 from django.conf.urls.static import static
 from django.http import HttpResponse
+from math import ceil
 
 from store.models import Product
 # Create your views here.
+
 def index(request):
-    var = Product.objects.all()
-    dict = {
-        'prodname': var,
-    }
-    return render(request, "store/index.html", dict)
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    return render(request, 'store/index.html', params)
+
+
+
+
+
 
 def about(request):
     return render(request, "store/about.html")
+
+
+
+
+
+
+
 
 #
 # def contact(request):
--- theawesomestore/settings.py
+++ theawesomestore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '12eas1qvtif&&*g_!dyp)gx9m3(q1rqrudct(&zkgrlx)2o6am'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '3.85.102.254', 'rudranshverma.ml']
 

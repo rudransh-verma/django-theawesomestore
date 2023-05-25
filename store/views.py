811019dbda465477148994edb9df23b932cc8e3e
Rudransh Verma <rudranshverma1984@gmail.com>
2023-05-25 20:30:15 +0530

    multiple category sliders

--- store/views.py
+++ store/views.py
@@ -7,11 +7,21 @@ from store.models import Product
 # Create your views here.
 
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    allProds = []
+    catProds = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catProds}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n//4 + ceil((n/4)-(n//4)) 
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+        
+    params = {
+        'allProds': allProds
+    }
+
+
     return render(request, 'store/index.html', params)
 
 
